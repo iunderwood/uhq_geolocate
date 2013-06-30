a63d0131c0490206523879bf4541a8b8177f2718
Ian Underwood <ian@underwood-hq.org>
2013-06-30 09:02:04 -0400

    Fix to prevent a PHP error, if the geolocation object has no country in the results (i.e. if there's an error condition)

--- blocks/uhqgeo_blocks.php
+++ blocks/uhqgeo_blocks.php
@@ -2,26 +2,23 @@
 
 require_once XOOPS_ROOT_PATH . "/modules/uhq_geolocate/class/geolocate.class.php";
 
+// This is a super simple block demonstrating how we can use this information.
 function b_uhqgeo_from_show($options) {
-	
-	// One of the simplest things we can do to use this.
-	
 	$geoloc = new geolocate;
-
 	$geoloc->ipin = $_SERVER['REMOTE_ADDR'];
+
 	$result = $geoloc->locate();
-	
+
 	$data['input'] = (array) $geoloc;
 	$data['result'] = (array) $result;
-	
+
 	if ($result) {
-		
-		include XOOPS_ROOT_PATH . "/modules/uhq_geolocate/includes/countryshort.php";
-		
-		$data['result']['flag'] = strtolower($data['result']['country']);
-		$data['result']['ccname'] = $_UHQGEO_CC[$data['result']['country']];
+		// Only process flags and names if we have a country.
+		if ($data['result']['country']) {
+			include XOOPS_ROOT_PATH . "/modules/uhq_geolocate/includes/countryshort.php";
+			$data['result']['flag'] = strtolower($data['result']['country']);
+			$data['result']['ccname'] = $_UHQGEO_CC[$data['result']['country']];
+		}
 	}
-	
 	return $data;
-		
 }
