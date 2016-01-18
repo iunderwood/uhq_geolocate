06a62f718dd604dc88986079f40ae653cf9257e5
mambax7 <mambax7@gmail.com>
2016-01-18 04:21:10 -0500

    updates

--- uhq_geolocate/xoops_version.php
+++ uhq_geolocate/xoops_version.php
@@ -33,8 +33,8 @@ $modversion["module_status"]       = "RC";
 
 // Minimums
 
-$modversion['min_php']      = '5.2';
-$modversion['min_xoops']    = "2.5.7";
+$modversion['min_php']      = '5.5';
+$modversion['min_xoops']    = "2.5.7.2";
 $modversion['min_admin']    = '1.1';
 $modversion['min_db']       = array(
     'mysql'  => '5.0.7',
